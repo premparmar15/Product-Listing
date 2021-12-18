47bcea71ad0e88e6c19560d885aa4d0bf9b91846
Prem <premparmar20357@gmail.com>
2021-12-18 12:50:26 +0530

    Last update

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { ActionTypes } from "../constants/action-types"
-import fakeApiStore from "../../apis/fakeApiStore";
 
 export const setProducts = (products) => {
     return {
@@ -10,9 +9,7 @@ export const setProducts = (products) => {
 }
 
 export const fetchProducts = () => async (dispatch) => {
-        //const response = await fakeApiStore.get('/products');
         const response = await axios.get('../../../api/cars.json');
-       // console.log(response1);
         dispatch({type: ActionTypes.FATCH_PRODUCTS, payload: response.data})
 }
 export const fetchProduct = (data) => async (dispatch) => {
